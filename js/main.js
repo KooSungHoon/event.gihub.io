d9217aac23350a3ec8173584f314c112041f652c
koosunghoon <kal9401@naver.com>
2023-04-14 01:10:51 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,18 @@
-// var audio = new Audio('etc/LoveBlossom.mp3');
-// audio.play();
-
-gsap.registerPlugin(ScrollTrigger);
-// Setup smooth scroll
-const scroller = document.querySelector('.scroller');
-const bodyScrollBar = Scrollbar.init(scroller, {damping: 0.07, delegateTo: document, alwaysShowTracks: false });
-ScrollTrigger.scrollerProxy(".scroller", {
-scrollTop(value) {
-    if (arguments.length) {
-    bodyScrollBar.scrollTop = value;
-    }
-    return bodyScrollBar.scrollTop;
+// bgm
+function play() {
+  document.getElementById("touch").style.display = "none";
+  var audio = document.getElementById("bgm");
+  audio.play();
+  init();
 }
-});
-bodyScrollBar.addListener(ScrollTrigger.update);
-ScrollTrigger.defaults({ scroller: scroller });
-
-
-gsap.to(".kakao", { duration: 5, width: '100%', ease: 'none', delay: 0});
-var sec1 = gsap.timeline();
-sec1.to(".msg1", {duration: 0.5, opacity: 1, padding: 0, ease: 'none', delay: 1});
-sec1.to(".msg1 p", {duration: 0, opacity: 0, ease: 'none', delay: 2});
-sec1.to(".msg2", {duration: 0.5, opacity: 1, padding: 0, ease: 'none', delay: 2});
-sec1.to(".kakao", {duration: 1, top: '-100vh', ease: 'power4.out', delay: 3});
 
 
 
+
+
+function init() {
+
+// 버티컬케러셀
 const additionalY = { val: 0 };
 let additionalYAnim;
 let offset = 0;
@@ -85,47 +72,73 @@ const imagesScrollerTrigger = ScrollTrigger.create({
   }
 });
 
+  // 카톡
+  gsap.to(".kakao", { duration: 5, width: '100%', ease: 'none', delay: 0});
+  var sec1 = gsap.timeline();
+  sec1.to(".msg1", {duration: 0.5, opacity: 1, padding: 0, ease: 'none', delay: 1});
+  sec1.to(".msg1 p", {duration: 0, opacity: 0, ease: 'none', delay: 2});
+  sec1.to(".msg2", {duration: 0.5, opacity: 1, padding: 0, ease: 'none', delay: 2});
+  sec1.to(".kakao", {duration: 1, top: '-100vh', ease: 'power4.out', delay: 3});
 
 
-// about us
-const sec2 = gsap.timeline({
+  
+
+
+    
+  // 편지
+  const sec2 = gsap.timeline({
     scrollTrigger: { 
         trigger : ".section4",
         scrub: true,
         pin: true,
         start: "top top",
-        markers:true,
+        // markers:true,
         end: "+=11520"         
     }
-});
-sec2.to(".text_about", {yPercent: -250, duration: 5}, "a");
-sec2.to(".text_us", {yPercent: 250, duration: 5}, "a");
-sec2.to(".box_01",{opacity:1, duration: 4});
-sec2.to(".t1",{opacity: 1, duration: 8});
-sec2.to(".history_box img",{opacity: 0, duration: 8});
-sec2.to(".t2",{opacity: 1, duration: 8});
-sec2.to(".history_box img",{opacity: 0, duration: 8});
-sec2.to(".t3",{opacity: 1, duration: 8});
-sec2.to(".history_box img",{opacity: 0, duration: 8});
-sec2.to(".t4",{opacity: 1, duration: 8});
+  });
+  sec2.to(".text_about", {yPercent: -250, duration: 5}, "a");
+  sec2.to(".text_us", {yPercent: 250, duration: 5}, "a");
+  sec2.to(".box_01",{opacity:1, duration: 4});
+  sec2.to(".t1",{opacity: 1, duration: 8});
+  sec2.to(".history_box img",{opacity: 0, duration: 8});
+  sec2.to(".t2",{opacity: 1, duration: 8});
+  sec2.to(".history_box img",{opacity: 0, duration: 8});
+  sec2.to(".t3",{opacity: 1, duration: 8});
+  sec2.to(".history_box img",{opacity: 0, duration: 8});
+  sec2.to(".t4",{opacity: 1, duration: 8});
 
-const sec2_2 = gsap.timeline({
+  const sec2_2 = gsap.timeline({
   scrollTrigger: { 
       trigger : ".section4",
       scrub: true,
       // pin: true,
       start: "top top",
-      markers:true,
+      // markers:true,
       end: "+=11520"         
   }
-});
-sec2_2.to(".section4", {'background-position': '0 -11544', duration: 5});
+  });
+  sec2_2.to(".section4", {'background-position': '0 -11544', duration: 5});
 
-if (document.querySelector('.gsap-marker-scroller-start')) {		
-    const markers = gsap.utils.toArray('[class *= "gsap-marker"]');	
 
-    bodyScrollBar.addListener(({ offset }) => {  
-        gsap.set(markers, { marginTop: -offset.y})
-    });
 }
+
+
+gsap.registerPlugin(ScrollTrigger);
+// Setup smooth scroll
+const scroller = document.querySelector('.scroller');
+const bodyScrollBar = Scrollbar.init(scroller, {damping: 0.07, delegateTo: document, alwaysShowTracks: false });
+ScrollTrigger.scrollerProxy(".scroller", {
+scrollTop(value) {
+    if (arguments.length) {
+    bodyScrollBar.scrollTop = value;
+    }
+    return bodyScrollBar.scrollTop;
+}
+});
+bodyScrollBar.addListener(ScrollTrigger.update);
+ScrollTrigger.defaults({ scroller: scroller });
+
+
+
+
 
